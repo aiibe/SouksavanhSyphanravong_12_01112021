fe07603379bd28290815d283d1cb3cc056f4b2cd
aiibe <souksavanh.sy@gmail.com>
2021-11-02 15:47:14 +0100

    Add landing

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,20 @@
+import { Link } from "react-router-dom";
+import "../css/Home.css";
+
 function Home() {
-  return <p>Choose a profile :</p>;
+  return (
+    <div className="landing">
+      <h1 className="landing__title">Choose a profile :</h1>
+      <ul className="landing__list">
+        <li className="landing__link">
+          <Link to="/user/12">See profile 12</Link>
+        </li>
+        <li className="landing__link">
+          <Link to="user/18">See profile 18</Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default Home;
